f8cc6d09361b7339bc84f9cef80e3b7e211e80e0
Chris Pound <c2.pound@gmail.com>
2015-06-09 20:51:16 -0700

    some ui changes.

--- app/src/main/java/com/chrispound/udacity/udacityportfolio/MainActivity.java
+++ app/src/main/java/com/chrispound/udacity/udacityportfolio/MainActivity.java
@@ -2,6 +2,7 @@ package com.chrispound.udacity.udacityportfolio;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -9,6 +10,7 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private Toast mToast;
+    private Toolbar mToolbar;
     private Button mSpotify;
     private Button mScores;
     private Button mLibrary;
@@ -16,21 +18,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private Button mXYZ;
     private Button mCapstone;
 
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
         mSpotify = (Button) findViewById(R.id.spotify);
         mScores = (Button) findViewById(R.id.scores);
         mLibrary = (Button) findViewById(R.id.library);
         mBuild = (Button) findViewById(R.id.build_bigger);
         mXYZ = (Button) findViewById(R.id.xyz);
         mCapstone = (Button) findViewById(R.id.myApp);
+        setSupportActionBar(mToolbar);
         mSpotify.setOnClickListener(this);
         mScores.setOnClickListener(this);
         mLibrary.setOnClickListener(this);
         mBuild.setOnClickListener(this);
         mXYZ.setOnClickListener(this);
         mCapstone.setOnClickListener(this);
+        setTitle(getString(R.string.app_name));
+        mToolbar.setTitleTextColor(getResources().getColor(android.R.color.white));
     }
 
     public void handleButtonClick(String clicked) {
